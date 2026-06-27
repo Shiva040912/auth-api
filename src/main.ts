cc8508a69f196d1d174a2fe90827715270c385b4
Shiva040912 <shivaarun0071@gmail.com>
2026-06-27 16:00:50 +0530

    fix: port and cors config for render

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
-  await app.listen(3000);
+  
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
-  console.log('Server running on http://localhost:3000');
+  console.log(`Server running on port ${port}`);
 }
 
 bootstrap();
